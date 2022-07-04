393b654ed558ac57e87b25662b6928382ca12ff9
aman.girma <girmaamanuel8@gmail.com>
2022-07-04 03:44:29 -0700

    new'

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-0-19\n");
+return (1);
+}
